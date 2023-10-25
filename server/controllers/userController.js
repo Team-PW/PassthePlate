5e99d013704595464a16fc4f64b59d747bebe9f2
Edward Li <edwardli744@gmail.com>
2023-10-25 13:12:19 -0700

    local list is now populating

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -8,12 +8,10 @@ userController.verifyUser = (req, res, next) => {
   // console.log('testing userReqBod: ', userReqBod)
   const queryString = `SELECT username, password, zipcode, id FROM "userinfo" WHERE username = '${req.body.username}' AND password = '${req.body.password}' AND zipcode = ${req.body.zipcode}`;
   db.query(queryString)
-    .then(data => {
-      console.log('data', data);
+    .then((data) => {
       // console.log('should log user inputted in login form', data.rows)
       if (data.rows.length !== 0) {
         // if user is found, redirect to home page (currently set to '/listings')
-        console.log('user found!', data.rows);
         res.locals.username = data.rows[0].username;
         res.locals.zipcode = data.rows[0].zipcode;
         res.locals.userID = data.rows[0].id;
@@ -23,22 +21,22 @@ userController.verifyUser = (req, res, next) => {
       // if user cannot be found, redirect to default path '/'
       return res.redirect('/');
     })
-    .catch(err => {
+    .catch((err) => {
       console.error('Error in verifyUser middleware: ', err);
     });
 };
 
 userController.findListings = (req, res, next) => {
   console.log('made it to findListings controller');
-  const queryString = `SELECT l.*, u.username FROM listing l inner join "user" u on u.id = l.user_id WHERE l.zipcode = ${req.cookies.zipcode}`;
+  const queryString = 'SELECT * FROM listing';
   // testing route handler for finding listing based on zipcode
   db.query(queryString)
-    .then(data => {
+    .then((data) => {
       console.log('data from listings', data.rows);
       res.locals.listings = data.rows;
       return next();
     })
-    .catch(err => console.error('Error in findListings middleware: ', err));
+    .catch((err) => console.error('Error in findListings middleware: ', err));
 
   // Query SQL DB for SELECT * WHERE userReqBody === zipcode_id
   // res.locals.listings = 'database response';
@@ -48,8 +46,8 @@ userController.postListing = (req, res, next) => {
   console.log('posting a listing thru postman: ', req.body);
   const queryString = `INSERT INTO listing (title, listing_body, zipcode, user_id) VALUES ('${req.body.title}', '${req.body.listingBody}', ${req.cookies.zipcode}, ${req.cookies.userID})`;
   db.query(queryString)
-    .then(data => next())
-    .catch(err => console.error('Error in postListing middleware: ', err));
+    .then((data) => next())
+    .catch((err) => console.error('Error in postListing middleware: ', err));
 };
 
 userController.postComment = (req, res, next) => {
@@ -62,13 +60,13 @@ userController.getComments = (req, res, next) => {
   const queryString = `SELECT comment_body FROM comment WHERE listing_id = ${req.cookies.id}`; // testing route handler for finding listing based on zipcode
 
   db.query(queryString)
-    .then(data => {
+    .then((data) => {
       // console.log('data from listings', data.rows)
 
       res.locals.comments = data.rows;
       return next();
     })
-    .catch(err => console.error('Error in findListings middleware: ', err));
+    .catch((err) => console.error('Error in findListings middleware: ', err));
 };
 
 userController.createUser = (req, res, next) => {
@@ -76,10 +74,10 @@ userController.createUser = (req, res, next) => {
   const queryString = 'SELECT username FROM "userinfo"';
   let currentLength;
   db.query(queryString)
-    .then(response => {
+    .then((response) => {
       currentLength = response.rowCount;
     })
-    .then(data => {
+    .then((data) => {
       const queryString1 = `INSERT INTO "userinfo"
     VALUES ('${req.body.username}', '${req.body.password}', ${
         req.body.zipcode
@@ -87,7 +85,7 @@ userController.createUser = (req, res, next) => {
       db.query(queryString1);
       return next();
     })
-    .catch(error => console.log('error:', error));
+    .catch((error) => console.log('error:', error));
   // if username already exists, redirect to default path '/'
   // if user is successfully created, redirect to home page
 
